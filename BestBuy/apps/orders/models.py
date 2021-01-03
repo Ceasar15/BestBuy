d3e9a9dad184b04315f8041a183a7a3454163ddc
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-03 14:46:43 +0000

    created order item model

--- BestBuy/apps/orders/models.py
+++ BestBuy/apps/orders/models.py
@@ -24,3 +24,14 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.item.all())
 
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+        
+    def get_cost(self):
+        return self.price * self.quantity
