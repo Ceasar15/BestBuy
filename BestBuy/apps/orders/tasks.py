d27d9992787489cbc848685d2c3e5f10acdb7d75
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-04 23:50:39 +0000

    adding asynchronous tasks to app

---
+++ BestBuy/apps/orders/tasks.py
@@ -0,0 +1,15 @@
+from celery import task 
+from django.core.mail import send_mail
+from .models import Order
+
+
+@task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f"Order nr. {order.id}"
+    message = f'Dear {order.first_name}, \n\n' \
+        f'You have successfully placed an order.' \
+        f'Your order ID is {order.id}.'
+    mail_sent = send_mail(subject, message, 'admin@bestbuy.com', [order.email])
+
+    return mail_sent
