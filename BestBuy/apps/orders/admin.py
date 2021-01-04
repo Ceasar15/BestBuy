19b457925d0d5219e29472ab38545ee7b729e1af
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-04 17:27:24 +0000

    included order models in admin site

--- BestBuy/apps/orders/admin.py
+++ BestBuy/apps/orders/admin.py
@@ -9,7 +9,7 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
-
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
