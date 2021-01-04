a6ac57a90fdbcc7f90f6f2e9d9534b65bd517e9d
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-04 17:26:56 +0000

    included order models in admin site

--- BestBuy/apps/orders/admin.py
+++ BestBuy/apps/orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+
 
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+
