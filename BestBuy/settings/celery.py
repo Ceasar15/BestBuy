a181af217fcd64640764ee113497651d3729f55d
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-05 13:36:24 +0000

    fixed database issues with celery

--- BestBuy/settings/celery.py
+++ BestBuy/settings/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery 
 import django.conf
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BestBuy.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BestBuy.settings.development')
 
 app = Celery('BestBuy')
 
