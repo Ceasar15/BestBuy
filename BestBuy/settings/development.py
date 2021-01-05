843eb21dfe9129bc7157ed4ef42cde93e7216b25
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-05 13:23:25 +0000

    database settings

--- BestBuy/settings/development.py
+++ BestBuy/settings/development.py
@@ -40,6 +40,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE': 600,
     }
 }
 # DATABASES = { 'default': dj_database_url.config( default=config('DATABASE_URL') ) }
