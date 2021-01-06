92b7ae488e9fbe373a70188ea51da3f8419ccdf7
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-06 15:04:10 +0000

    retrieving posts by similarity

--- BestBuy/apps/products/views.py
+++ BestBuy/apps/products/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count
 from django.http import request, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView, DetailView
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
+
 
 from taggit.models import Tag
 
