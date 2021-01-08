ec8cc08989b471e374c1c3ae18d3cde4cb4ee12e
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-08 20:39:24 +0000

    Adding custom actions to the administration site

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
@@ -9,7 +10,10 @@ from .tasks import order_created
 
 
 @staff_member_required
-def
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders//order/detail.html')
+    
 
 def order_create(request):
     cart = Cart(request)
