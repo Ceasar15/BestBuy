a61b1cbbfe57076aa29b82ef966edebafd834dd4
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-08 20:28:17 +0000

    Adding custom actions to the administration site

--- BestBuy/apps/orders/admin.py
+++ BestBuy/apps/orders/admin.py
@@ -9,6 +9,30 @@ from .models import Order, OrderItem
 
 # Register your models here.
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = 'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # The data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+                data_row.append(value)
+                writer.writerow(data_row)
+    return response
+
+export_to_csv.short_description = 'Export to CSV'
+
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
