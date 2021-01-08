0cecc5176099aed9f952d7b9b65e12bf891da327
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-08 20:24:36 +0000

    Adding custom actions to the administration site

--- BestBuy/apps/orders/admin.py
+++ BestBuy/apps/orders/admin.py
@@ -29,3 +29,15 @@ def export_to_csv(modeladmin, request, queryset):
     fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
     # Write a first row with header information
     writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+                data_row.append(value)
+                writer.writerow(data_row)
+    return response
+
+    
