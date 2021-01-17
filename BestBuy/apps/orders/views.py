86f21681cf7b2c38a413f3bc681282d8b21a849b
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-17 15:47:12 +0000

    fixed it

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-#import weasyprint
+import weasyprint
 
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
@@ -50,5 +50,5 @@ def admin_order_pdf(request, order_id):
     html = render_to_string('orders/order/pdf.html', {'order': order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    #weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
     return response
