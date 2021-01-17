dd5529fab84f981fec5148a50bfa638feb45ec4b
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-17 15:42:23 +0000

    some error bi i dont kmow

--- BestBuy/apps/orders/admin.py
+++ BestBuy/apps/orders/admin.py
@@ -48,7 +48,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'paid', 'country', 'postal_code', 'city', 'street' , 'building', 'created', 'updated', order_detail]
+    list_display = ['id', 'first_name', 'last_name', 'email', 'paid', 'country', 'postal_code', 'city', 'street' , 'building', 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
