2b9cbd7dbc04bb1e56c2dbdd218ffba81e545d77
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-17 15:05:56 +0000

    rendering pdf files

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -51,5 +51,4 @@ def admin_order_pdf(request, order_id):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
-    
     return response
