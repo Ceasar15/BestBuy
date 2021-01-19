97b61512f53d6a2c8b0f720acbae0e8b097a31f1
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-19 15:18:15 +0000

    creating a coupon system

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -50,5 +50,5 @@ def admin_order_pdf(request, order_id):
     html = render_to_string('orders/order/pdf.html', {'order': order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + '/0css/pdf.css')])
     return response
