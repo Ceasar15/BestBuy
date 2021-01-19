11fa450e82e262fa4a04116d4d405ae764418c70
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-19 15:40:02 +0000

    applying coupon to shopping cart

--- BestBuy/apps/coupons/views.py
+++ BestBuy/apps/coupons/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .mode
 
 # Create your views here.
+
