9213e67e119ea026266b63c9bba1cf68feb189fc
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-20 17:29:36 +0000

    applying coupon in html

--- BestBuy/apps/orders/models.py
+++ BestBuy/apps/orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from apps.products.models import Product
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from apps.coupons.models import Coupon
 
 # Create your models here.
 
@@ -16,6 +19,8 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon, null=True, blank=True, related_name='orders', on_delete=models.CASCADE)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
