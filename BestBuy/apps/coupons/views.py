3b9b5603c2d20d75d948520cd9d8a7a41d0d4740
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-21 19:45:27 +0000

    coupon render

--- BestBuy/apps/coupons/views.py
+++ BestBuy/apps/coupons/views.py
@@ -13,7 +13,7 @@ def coupon_apply(request):
     if form.is_valid():
         code = form.cleaned_data['code']
         try:
-            coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__get=now, activate=True)
+            coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__gte=now, active=True)
             request.session['coupon_id'] = coupon.id
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
