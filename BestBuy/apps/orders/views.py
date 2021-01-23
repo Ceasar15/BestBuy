54887c14839bf1f99b82164d241d3bca3cf55d7d
Ceasar15 <ceasarkwadwo@gmail.com>
2021-01-23 15:21:46 +0000

    finished add coupon system

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -56,4 +56,3 @@ def admin_order_pdf(request, order_id):
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + '/css/pdf.css')])
     return response
-zxbvjsd vsdivns\
