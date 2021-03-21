4ba45f335025fc1989dc06e9e165ee3303c0bb7c
Ceasar15 <ceasarkwadwo@gmail.com>
2021-03-21 21:22:02 +0000

    deploying issues

--- BestBuy/settings/development.py
+++ BestBuy/settings/development.py
@@ -11,8 +11,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
 #     dotenv.load_dotenv(dotenv_file)
 
 ########## DEBUG CONFIGURATION
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python3 manage.py migrate
+release: python manage.py migrate
 web: gunicorn BestBuy.wsgi --log-file -
