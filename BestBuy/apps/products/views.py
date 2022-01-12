e5755a70238bcd7731995b1eb11c7b9bcfd83abf
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-12 10:17:17 +0000

    home page refactor

--- BestBuy/apps/products/views.py
+++ BestBuy/apps/products/views.py
@@ -112,31 +112,7 @@ class ProductDetailSlugView(DetailView):
 
  
 
-# class ProductDetailView(DetailView):
-#     queryset = Product.objects.all()
-#     template_name = "products/detail.html"
 
-#     def get_context_data(self, *args, **kwargs):
-#         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-#         print(context)
-#         # context['abc'] = 123
-#         return context
-
-#     def get_object(self, *args, **kwargs):
-#         request = self.request
-#         pk = self.kwargs.get('pk')
-#         instance = Product.objects.get_by_id(pk)
-#         if instance is None:
-#             raise Http404("Product doesn't exist")
-#         return instance
-
-#     # def get_queryset(self, *args, **kwargs):
-#     #     request = self.request
-#     #     pk = self.kwargs.get('pk')
-#     #     return Product.objects.filter(pk=pk)
-#     def get_queryset(self, *args, **kwargs):
-#         request = self.request
-#         return Product.objects.all()
 
 def product_detail_view(request, slug, *args, **kwargs):
     queryset = Product.objects.all()
