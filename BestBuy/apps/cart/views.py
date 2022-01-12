4b44a8615994bb83c385f1e45bcd0b4f3ed6daf3
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-12 10:35:53 +0000

    home page refactor

--- BestBuy/apps/cart/views.py
+++ BestBuy/apps/cart/views.py
@@ -11,6 +11,7 @@ from .forms import CartAddProductForm
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
+    print(cart)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
