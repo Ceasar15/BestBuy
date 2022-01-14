d8abf22070495d07e152360f5d6e8a5ff8e66a7e
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-14 11:48:32 +0000

    list category filter

--- BestBuy/apps/products/urls.py
+++ BestBuy/apps/products/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('products/', views.product_list_view, name='products_list'),
     path('tag/<slug:tag_slug>/', views.product_list_view, name='product_list_by_tag'),
     path('filter/<slug:cat_filter>/', views.product_list_view, name="product_list_by_filter"),
-    path('products/<int:id>/<slug:slug>/', views.product_detail_view),
+    path('products/<int:id>/<slug:slug>/', views.product_detail_view, name="product_list_detail"),
 #    path('<slug:slug>/', views.ProductDetailSlugView.as_view()),
 ]
