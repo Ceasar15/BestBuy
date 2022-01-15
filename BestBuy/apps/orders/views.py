9dd4c068edad75f44167d5ea0ecc6a875b3dd549
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-15 08:37:58 +0000

    order pdf view worker config

--- BestBuy/apps/orders/views.py
+++ BestBuy/apps/orders/views.py
@@ -1,4 +1,3 @@
-from django.http import request
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
@@ -54,5 +53,5 @@ def admin_order_pdf(request, order_id):
     html = render_to_string('orders/order/pdf.html', {'order': order})
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + '/css/pdf.css')])
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS( 'BestBuy/' + settings.STATIC_URL + 'css/pdf.css')])
     return response
