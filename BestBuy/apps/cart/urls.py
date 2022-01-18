a4af55f0c03672bf4139145c6fc1f685d86539d8
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-18 14:53:27 +0000

    celery config mess

--- BestBuy/apps/cart/urls.py
+++ BestBuy/apps/cart/urls.py
@@ -1,6 +1,5 @@
 
 from django.urls import path
-# from django.conf.urls import url
 from . import views
 
 app_name = 'cart'
--- BestBuy/apps/products/urls.py
+++ BestBuy/apps/products/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 # from django.conf.urls import url
-from django.conf.urls import url
 
 from . import views
 
