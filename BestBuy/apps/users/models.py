ee3f305b230ea0b3f19ee3c9456f8a0bdc44784a
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-18 14:16:46 +0000

    app registry issues

--- BestBuy/apps/users/models.py
+++ BestBuy/apps/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
 
 from .managers import CustomUserManager
 
@@ -21,40 +23,4 @@ class CustomUser(AbstractUser):
       
       def __str__(self): 
         return "{}".format(self.email)             
-
-
-
-
-
-
-
-
-
-
-
-
-# class CustomUser(AbstractUser):
-#     email = models.EmailField(max_length=50, null=True)
-#     phone = models.CharField(max_length=15, null=True)
-#     first_name = models.CharField(max_length=100, null=True)
-#     last_name = models.CharField(max_length=100, null=True)
-#     last_login = models.DateTimeField(auto_now_add=True, null=True)    
-    
-
-#     def __str__(self): 
-#       return "{}".format(self.email) 
-
-
-# @receiver(post_save, sender=AbstractUser)
-# def create_profile(sender, instance, created, **kwargs):
-#         if created:
-#             CustomUser.objects.create(user=instance)
-    
-# @receiver(post_save, sender=AbstractUser)
-# def save_profile(sender, instance, **kwargs):
-#         instance.profile.save()
-
-
-
-
 
