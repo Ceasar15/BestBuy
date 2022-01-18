74a591e5451c59fcbcad32078ce8c01edc203849
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-18 14:27:14 +0000

    app registry issues

--- BestBuy/apps/cart/urls.py
+++ BestBuy/apps/cart/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
 #    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 
--- BestBuy/apps/users/urls.py
+++ BestBuy/apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
     path('sign_up/', views.SignUp.as_view(), name='sign_up'),
  #   path('login/', views.login, name='login'),
-    url(r"^", include("django.contrib.auth.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
