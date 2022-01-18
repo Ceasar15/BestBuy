75a8dda520fd250efb20e469567492eed4186ae2
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-18 11:56:33 +0000

    fixed all issues

--- BestBuy/apps/orders/tasks.py
+++ BestBuy/apps/orders/tasks.py
@@ -1,8 +1,6 @@
-from django.core.mail import send_mail
 from django.core.mail.message import EmailMessage
 from .models import Order
 from BestBuy.settings.celery import app
-
 
 @app.task()
 def order_created(order_id):
@@ -11,7 +9,7 @@ def order_created(order_id):
     message = f'Dear {order.first_name}, \n\n' \
         f'You have successfully placed an order.' \
         f'Your order ID is {order.id}.'
-    # mail_sent = send_mail(subject, message, 'ceasarkwadwo@gmail.com', [order.email])
+            
     email = EmailMessage (
         subject=subjectHead,
         body=message,
