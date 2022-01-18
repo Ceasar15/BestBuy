6d29700aa97cbd0de91aabaed8eba7bf845d12be
Ceasar15 <ceasarkwadwo@gmail.com>
2022-01-18 14:45:36 +0000

    dependecy issues par fixed

--- BestBuy/apps/products/urls.py
+++ BestBuy/apps/products/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
+# from django.conf.urls import url
 from django.conf.urls import url
 
 from . import views
 
 
-#app_name = 'products'
+app_name = 'products'
 
 urlpatterns = [
 
     path('catalog/', views.CatalogView.as_view(), name='products_catalog'),
     path('recom/', views.recom_list_view, name='recom_products'),
     path('gallery/', views.GalleryView.as_view(), name='products_gallery'),
-#    url(r'^$', views.ProductListView.as_view(), name='products_list'),
     path('products/', views.product_list_view, name='products_list'),
     path('tag/<slug:tag_slug>/', views.product_list_view, name='product_list_by_tag'),
     path('filter/<slug:cat_filter>/', views.product_list_view, name="product_list_by_filter"),
--- BestBuy/apps/users/urls.py
+++ BestBuy/apps/users/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.conf.urls import include
 from . import views
 
+app_name = 'users'
+
+
 urlpatterns = [
     path('sign_up/', views.SignUp.as_view(), name='sign_up'),
  #   path('login/', views.login, name='login'),
