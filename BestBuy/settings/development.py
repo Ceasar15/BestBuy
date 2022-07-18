cc04ea429cbd9cfa31af0908ed09824e8fa969b8
Ceasar <ceasarkwadwo@gmail.com>
2022-07-18 09:44:23 +0000

    development database config

--- BestBuy/settings/development.py
+++ BestBuy/settings/development.py
@@ -28,10 +28,10 @@ TEMPLATE_DEBUG = DEBUG
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ceasarkwadwo@gmail.com'
-EMAIL_HOST_PASSWORD = 'Edem12345'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'ceasarkwadwo@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 ########## END EMAIL CONFIGURATION
 
 # Database
